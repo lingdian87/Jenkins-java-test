c5a2524f3e66f8eac5c588903a69dc15626fde1d
Hu,Shenggen <shenggenx.hu@intel.com>
2014-12-15 16:51:17 +0800

    add a line to jenkins

--- helloworld.java
+++ helloworld.java
@@ -8,5 +8,6 @@
 public class helloworld{
         public static void main(String[] args) {
                     System.out.println("Hello World");
+                    System.out.println("Add a word");
                         }
 }
