7f21a699b3c45a1a800cf5fd041b4c3dd254b6a5
Hu,Shenggen <shenggenx.hu@intel.com>
2014-12-15 13:05:48 +0800

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: helloworld.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2014年12月15日 星期一 11时32分06秒
+ ************************************************************************/
+
+public class helloworld{
+        public static void main(String[] args) {
+                    System.out.println("Hello World");
+                        }
+}
